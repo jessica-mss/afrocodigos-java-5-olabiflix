ed7a43d407b94759bb4a370249264d15a73fe222
Jessica <jessica-mss@hotmail.com>
2024-05-22 21:15:29 -0300

    feat: creating a test endpoint

--- src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
+++ src/main/java/com/olabi/olabiflix/OlabiflixApplication.java
@@ -2,12 +2,22 @@ package com.olabi.olabiflix;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/olabiflix")
 public class OlabiflixApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(OlabiflixApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	public String hello() {
+		return "Hello World";
 	}
 
 }
